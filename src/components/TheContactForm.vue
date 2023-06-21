d86de150cccbd2523e0fd50b4ee7c2fc8efc1d6d
濵田広大 <hamany0326@gmail.com>
2023-06-22 02:17:30 +0900

    Add: contact page

--- src/components/TheContactForm.vue
+++ src/components/TheContactForm.vue
@@ -39,8 +39,77 @@ const onSubmit = (): void => {};
     </div>
     <div>
       <label class="block">
-        <sapn class="">貴社名</sapn>
+        <sapn class="text-base">貴社名</sapn>
+        <input
+          class="block mt-4 pl-4 py-4 w-full border border-primary rounded"
+          placeholder="例）株式会社アンドピリオド"
+        />
       </label>
+    </div>
+    <div>
+      <label class="block">
+        <sapn class="text-base">お名前</sapn>
+        <span
+          class="border ml-4 px-4 py-1 text-[15px] text-primary border-primary rounded-full"
+          >必須</span
+        >
+        <input
+          class="block mt-4 pl-4 py-4 w-full border border-primary rounded"
+          placeholder="例）山田 太郎"
+        />
+      </label>
+    </div>
+    <div>
+      <label class="block">
+        <sapn class="text-base">メールアドレス</sapn>
+        <span
+          class="border ml-4 px-4 py-1 text-[15px] text-primary border-primary rounded-full"
+          >必須</span
+        >
+        <input
+          class="block mt-4 pl-4 py-4 w-full border border-primary rounded"
+          placeholder="例）example@and-period.co.jp"
+        />
+      </label>
+    </div>
+    <div>
+      <label class="block">
+        <sapn class="text-base">電話番号（ハイフンなし）</sapn>
+        <span
+          class="border ml-4 px-4 py-1 text-[15px] text-primary border-primary rounded-full"
+          >必須</span
+        >
+        <input
+          class="block mt-4 pl-4 py-4 w-full border border-primary rounded"
+          placeholder="例）090XXXXXXXX"
+        />
+      </label>
+    </div>
+    <div>
+      <label class="block">
+        <sapn class="text-base">お問い合わせ詳細</sapn>
+        <span
+          class="border ml-4 px-4 py-1 text-[15px] text-primary border-primary rounded-full"
+          >必須</span
+        >
+        <textarea
+          class="block mt-4 pl-4 pt-4 py-[400px] w-full border border-primary rounded"
+          placeholder="お問い合わせ内容を具体的にご記入ください"
+        ></textarea>
+      </label>
+    </div>
+    <div class="text-center whitespace-nowrap">
+      <p>
+        <u><a href="/privacy">プライバシーポリシー</a></u
+        >をご確認いただき、同意いただける場合のみお進みください。
+      </p>
+    </div>
+    <div class="flex justify-center mb-20">
+      <nuxt-link
+        class="block max-w-max bg-primary text-white rounded-full py-4 px-[80px] text-l hover:bg-opacity-80"
+      >
+        同意して送信する
+      </nuxt-link>
     </div>
   </div>
 </template>
