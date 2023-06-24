0ef68b74f1c9dfca89759edd43e6e00c5135153e
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2023-06-24 21:23:28 +0900

    feat: ヘッダーにsticky属性を追加

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-default flex flex-col h-full">
-    <the-app-header />
+    <the-app-header class="sticky top-0 z-20" />
     <div class="grow flex justify-center">
       <div class="max-w-[1200px] w-full">
         <slot />
