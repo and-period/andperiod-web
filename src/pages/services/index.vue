8960a8caa771b0cfb2836301a1dfa8d8e606653e
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2023-06-25 22:48:37 +0900

    wip

--- src/pages/services/index.vue
+++ src/pages/services/index.vue
@@ -51,16 +51,59 @@
     <div
       class="bg-white rounded-3xl w-full flex flex-col py-8 px-6 gap-y-10 items-center md:py-20 md:px-24"
     >
-      <div class="text-left font-medium whitespace-nowrap">
+      <div class="w-full text-left font-medium whitespace-nowrap">
         <h2
           class="text-primary text-[32px] md:text-[56px] tracking-widest mb-2"
           id="furumaru"
         >
-          On-line marche
+          Online marche
         </h2>
         <p class="text-[13px] md:text-xl tracking-widest">
           オンラインマルシェ事業
         </p>
+      </div>
+      <div class="flex gap-x-[126px] items-center">
+        <services-the-furumaru-logo />
+        <div class="flex flex-col gap-6">
+          <p
+            class="text-xl whitespace-pre-wrap"
+            v-text="
+              '生産者のこだわりが\n「伝える」以上に「伝わる」ライブマルシェ'
+            "
+          />
+          <h2 class="text-[32px]">ふるマル</h2>
+        </div>
+      </div>
+
+      <div class="flex gap-x-11">
+        <div class="flex flex-col gap-6">
+          <h2 class="text-2xl">
+            どこからでもリアルタイムで日本の「食」を体験できる仮想空間の実現
+          </h2>
+          <p class="tex-xl">
+            わたしたちの目指す産直ECの姿は安心して商品を購入できライブコマースを通じてリアルに体験することでお互いの想いを直接感じられるプラットフォームです。ライブマルシェは双方向でのコミュニケーションが図れるので農家さんのこだわりを直接お伝えできるとともに視聴者はライブ中に気になるポイントを質問することで、ワクワク感や納得感を得ながらお買い物をより楽しむことができます。
+          </p>
+        </div>
+        <img
+          src="/img/services/furumaru/1.png"
+          alt="furumaru"
+          class="object-contain max-w-[320px]"
+        />
+      </div>
+
+      <div class="flex gap-x-11">
+        <img
+          src="/img/services/furumaru/2.png"
+          alt=""
+          class="object-contain max-w-[320px]"
+        />
+        <div lass="flex flex-col gap-6">
+          <h2 class="text-2xl">現地の空気感をオンタイムで味わう非日常体験</h2>
+          <p class="tex-xl">
+            ライブマルシェを通じて自分がその場で気に入った商品を購入できるという疑似体験によって臨場感が味わえるので、マルシェならではのワクワクや充実感を感じることができます。
+            また、既存システムにはない新しい購買体験（ピッキング機能）は実際にマルシェに赴いたかのようなお買い物の楽しさをリアルに再現できます。
+          </p>
+        </div>
       </div>
     </div>
   </div>
