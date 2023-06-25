685075d5ee993718a6063ca5ae8dfea2f1bc7c6f
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2023-06-25 23:40:28 +0900

    feat: ふるマルのサービス紹介をサービスについてページに追加

--- src/pages/services/index.vue
+++ src/pages/services/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="lg:px-10 px-6">
+  <div class="lg:px-10 px-6 pb-6">
     <div class="my-20 flex lg:flex-row flex-col lg:gap-20 gap-10">
       <div class="text-left font-medium whitespace-nowrap">
         <h2
@@ -49,7 +49,7 @@
     </div>
 
     <div
-      class="bg-white rounded-3xl w-full flex flex-col py-8 px-6 gap-y-10 items-center md:py-20 md:px-24"
+      class="bg-white rounded-3xl w-full flex flex-col py-8 px-6 gap-y-[120px] items-center md:py-20 md:px-24"
     >
       <div class="w-full text-left font-medium whitespace-nowrap">
         <h2
@@ -62,9 +62,12 @@
           オンラインマルシェ事業
         </p>
       </div>
-      <div class="flex gap-x-[126px] items-center">
-        <services-the-furumaru-logo />
-        <div class="flex flex-col gap-6">
+
+      <div class="flex gap-11 items-center w-full lg:flex-row flex-col">
+        <div class="lg:w-1/2 w-full m-auto">
+          <services-the-furumaru-logo class="w-full" />
+        </div>
+        <div class="lg:w-1/2 w-full flex flex-col gap-6">
           <p
             class="text-xl whitespace-pre-wrap"
             v-text="
@@ -75,8 +78,10 @@
         </div>
       </div>
 
-      <div class="flex gap-x-11">
-        <div class="flex flex-col gap-6">
+      <div
+        class="flex w-full gap-11 items-center lg:w-full lg:flex-row flex-col"
+      >
+        <div class="lg:w-1/2 w-full flex flex-col gap-6 lg:order-1 order-2">
           <h2 class="text-2xl">
             どこからでもリアルタイムで日本の「食」を体験できる仮想空間の実現
           </h2>
@@ -87,23 +92,40 @@
         <img
           src="/img/services/furumaru/1.png"
           alt="furumaru"
-          class="object-contain max-w-[320px]"
+          class="object-contain lg:w-1/2 w-full lg:order-2 order-1"
         />
       </div>
 
-      <div class="flex gap-x-11">
+      <div class="flex w-full gap-11 lg:flex-row flex-col">
         <img
           src="/img/services/furumaru/2.png"
           alt=""
-          class="object-contain max-w-[320px]"
+          class="object-contain lg:w-1/2 w-full"
         />
-        <div lass="flex flex-col gap-6">
+        <div class="flex flex-col gap-6 lg:w-1/2 w-full">
           <h2 class="text-2xl">現地の空気感をオンタイムで味わう非日常体験</h2>
           <p class="tex-xl">
             ライブマルシェを通じて自分がその場で気に入った商品を購入できるという疑似体験によって臨場感が味わえるので、マルシェならではのワクワクや充実感を感じることができます。
             また、既存システムにはない新しい購買体験（ピッキング機能）は実際にマルシェに赴いたかのようなお買い物の楽しさをリアルに再現できます。
           </p>
         </div>
+      </div>
+
+      <div
+        class="flex w-full gap-11 items-center lg:w-full lg:flex-row flex-col"
+      >
+        <div class="lg:w-1/2 w-full flex flex-col gap-6 lg:order-1 order-2">
+          <h2 class="text-2xl">産地コーディネーター制度</h2>
+          <p class="tex-xl">
+            「出品・配信・ヒアリング・検品・梱包・出荷」といった物流倉庫機能を一括して行うコーディネーターという役割を設けます。コーディネーターが介在することで生産者の作業負担を軽減できます。また購入にあたっては自分が気に入ったものを複数の生産者の方から少量ずつ購入できます。これにより生産者は栽培に集中することが可能となり、これまで障壁の高かった産直ECに手軽に参入することも可能となります。好きなものを好きな量注文することができるため、１回の配送で産地の特産品セットを自宅に届けることができます。
+            ライブマルシェを通じて自分がその場で気に入った商品を購入できるという疑似体験によって臨場感が味わえるので、マルシェならではのワクワクや充実感を感じることができます。
+          </p>
+        </div>
+        <img
+          src="/img/services/furumaru/3.png"
+          alt=""
+          class="object-contain lg:w-1/2 w-full lg:order-2 order-1"
+        />
       </div>
     </div>
   </div>
