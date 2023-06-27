c1f752e7979f3ec9218db4ebbef11f568d669202
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2023-06-27 23:56:07 +0900

    feat: ページ上部のデザインを修正

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="w-full flex flex-col gap-10 mb-10 2xl:px-0 p-6">
+    <div class="my-20 flex lg:flex-row flex-col lg:gap-20 gap-10">
+      <div class="text-left font-medium whitespace-nowrap">
+        <h2
+          class="text-primary text-[32px] md:text-[56px] tracking-widest mb-2 font-outfit"
+        >
+          About us
+        </h2>
+        <p class="text-[13px] md:text-xl tracking-widest">会社概要</p>
+      </div>
+      <div>
+        <h1
+          class="whitespace-pre-wrap lg:min-w-max font-medium text-[32px] tracking-widest"
+          v-text="'継承主体から、\n新たな一次産業をともに創造する未来へ。'"
+        />
+        <p class="mt-10 text-[20px] leading-[48px] tracking-widest">
+          「いいもの」だけが残る好循環を目指し、一次産業とITを融合した新しい基盤•仕組み•文化を創出します。
+        </p>
+      </div>
+    </div>
+
     <services-the-service-content-box title="Company" subtitle="会社概要">
       <div class="w-full border-t border-b border-gray border-dashed text-xl">
         <dl class="divide-y divide-gray divide-dashed">
