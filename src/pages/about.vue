b5cbec74f9b3298525456fda902cf0656cb86d93
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2023-06-28 15:42:20 +0900

    feat: スマホレイアウトの調整

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,6 +1,21 @@
+<script setup lang="ts">
+const ABOUT_US_ITEMS = [
+  { key: '会社名', value: '株式会社アンドピリオド' },
+  { key: '設立年月日', value: '2019年8月1日' },
+  { key: '本社所在地', value: '広島県豊田郡大崎上島町中野5748番地2' },
+  { key: '資本金', value: '4,865千円' },
+  { key: '代表者', value: '代表取締役社長 藤中拓弥' },
+  { key: '従業員数', value: '9名' },
+  {
+    key: '事業内容',
+    value: 'ライブコマース・フードロス・卸売・農業・Eコマース',
+  },
+];
+</script>
+
 <template>
   <div class="w-full flex flex-col gap-10 mb-10 2xl:px-0 p-6">
-    <div class="my-20 flex lg:flex-row flex-col lg:gap-20 gap-10">
+    <div class="lg:my-20 flex lg:flex-row flex-col lg:gap-20 gap-10">
       <div class="text-left font-medium whitespace-nowrap">
         <h2
           class="text-primary text-[32px] md:text-[56px] tracking-widest mb-2 font-outfit"
@@ -21,43 +36,17 @@
     </div>
 
     <services-the-service-content-box title="Company" subtitle="会社概要">
-      <div class="w-full border-t border-b border-gray border-dashed text-xl">
+      <div
+        class="w-full border-t border-b border-gray border-dashed md:text-xl text-[13px]"
+      >
         <dl class="divide-y divide-gray divide-dashed">
-          <div class="grid grid-cols-3 gap-x-4 py-8 md:px-10 px-2">
-            <dt>会社名</dt>
-            <dd class="col-span-2">株式会社アンドピリオド</dd>
-          </div>
-
-          <div class="grid grid-cols-3 gap-x-4 py-8 md:px-10 px-2">
-            <dt>設立年月日</dt>
-            <dd class="col-span-2">2019年8月1日</dd>
-          </div>
-
-          <div class="grid grid-cols-3 gap-x-4 py-8 md:px-10 px-2">
-            <dt>本社所在地</dt>
-            <dd class="col-span-2">広島県豊田郡大崎上島町中野5748番地2</dd>
-          </div>
-
-          <div class="grid grid-cols-3 gap-x-4 py-8 md:px-10 px-2">
-            <dt>資本金</dt>
-            <dd class="col-span-2">4,865千円</dd>
-          </div>
-
-          <div class="grid grid-cols-3 gap-x-4 py-8 md:px-10 px-2">
-            <dt>代表者</dt>
-            <dd class="col-span-2">代表取締役社長 藤中拓弥</dd>
-          </div>
-
-          <div class="grid grid-cols-3 gap-x-4 py-8 md:px-10 px-2">
-            <dt>従業員数</dt>
-            <dd class="col-span-2">9名</dd>
-          </div>
-
-          <div class="grid grid-cols-3 gap-x-4 py-8 md:px-10 px-2">
-            <dt>事業内容</dt>
-            <dd class="col-span-2">
-              ライブコマース・フードロス・卸売・農業・Eコマース
-            </dd>
+          <div
+            v-for="(item, i) in ABOUT_US_ITEMS"
+            :key="i"
+            class="grid grid-cols-3 gap-x-4 gap-y-4 md:py-8 py-6 md:px-10 px-2"
+          >
+            <dt class="md:col-span-1 col-span-3">{{ item.key }}</dt>
+            <dd class="md:col-span-2 col-span-3">{{ item.value }}</dd>
           </div>
         </dl>
       </div>
@@ -65,7 +54,7 @@
       <img
         src="/img/about/1.png"
         alt="株式会社アンドピリオドのメンバー写真"
-        class="w-full"
+        class="w-full mt-10"
       />
     </services-the-service-content-box>
   </div>
