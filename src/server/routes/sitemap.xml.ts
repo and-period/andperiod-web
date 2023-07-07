deecb2dcaacea0e1969f9a6d49ba6ad033317f1f
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2023-07-07 23:44:50 +0900

    feat: sitemapを追加

--- src/server/routes/sitemap.xml.ts
+++ src/server/routes/sitemap.xml.ts
@@ -8,6 +8,18 @@ export default defineEventHandler(async (event) => {
     hostname: 'https://www.and-period.co.jp/',
   });
 
+  const pages = [
+    { url: '/' },
+    { url: '/about' },
+    { url: '/contact' },
+    { url: '/news' },
+    { url: '/services' },
+  ];
+
+  for (const page of pages) {
+    sitemap.write({ ...page });
+  }
+
   for (const doc of docs) {
     sitemap.write({
       url: doc._path,
