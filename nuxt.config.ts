4a5e4fa3c959b7cbac3af0a0bbb5b2948ab01974
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2023-07-09 23:27:44 +0900

    feat: theme-colorを設定する

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,9 +7,13 @@ export default defineNuxtConfig({
       title: '株式会社アンドピリオド and period',
       meta: [
         {
+          name: 'theme-color',
+          content: '#64C4D7',
+        },
+        {
           name: 'description',
           content:
-            'and periodはライブ配信で全国の「ふるさと」を感じながら新しい購買体験が可能となるオンラインマルシェサービス「ふるマル」を中心としたシステム開発事業・販路開拓事業・フードロス対策事業など生産者・消費者双方に寄り添ったサービスの展開を行っています。',
+            '株式会社アンドピリオド（and period）はライブ配信で全国の「ふるさと」を感じながら新しい購買体験が可能となるオンラインマルシェサービス「ふるマル」を中心としたシステム開発事業・販路開拓事業・フードロス対策事業など生産者・消費者双方に寄り添ったサービスの展開を行っています。',
         },
         { property: 'og:type', content: 'website' },
         { property: 'og:site_name', content: 'and period' },
